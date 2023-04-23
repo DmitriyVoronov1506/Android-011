5ae68e8cc4c6b2bcecfea54b6d8d51372fad920e
DmitriyVoronov1506 <dmitriy.voronov1506@gmail.com>
2023-04-23 18:23:39 +0300

    CW Chat3

--- app/src/main/java/step/learning/course/ChatActivity.java
+++ app/src/main/java/step/learning/course/ChatActivity.java
@@ -1,11 +1,15 @@
 package step.learning.course;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -16,6 +20,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
@@ -32,8 +38,9 @@ public class ChatActivity extends AppCompatActivity {
     private final String CHAT_URL = "https://diorama-chat.ew.r.appspot.com/story";
     private EditText etAuthor;
     private EditText etMessage;
-    private TextView tvChat;
+    private LinearLayout chatContainer;
 
+    private ScrollView svContainer;
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
     @Override
@@ -44,7 +51,9 @@ public class ChatActivity extends AppCompatActivity {
 
         etAuthor = findViewById(R.id.chat_et_author);
         etMessage = findViewById(R.id.chat_et_message);
-        tvChat = findViewById(R.id.tv_chat);
+        chatContainer = findViewById(R.id.chat_container);
+        svContainer = findViewById(R.id.sv_Container);
+
         findViewById(R.id.chat_button_send).setOnClickListener(this::sendMessageClick);
 
         new Thread(this::getChatMessages).start();
@@ -82,6 +91,7 @@ public class ChatActivity extends AppCompatActivity {
         try {
 
             JSONObject content = new JSONObject(loadedContent);
+            boolean wasNewMessage = false;
 
             if(content.has("data")) {
 
@@ -90,12 +100,21 @@ public class ChatActivity extends AppCompatActivity {
 
                 for(int i = 0; i < len; i++) {
 
-                   chatMessages.add(
-                           new ChatMessage(data.getJSONObject(i))
-                   );
+                    ChatMessage tmp = new ChatMessage(data.getJSONObject(i));
+
+                    if(this.chatMessages.stream().noneMatch(msg -> msg.getId().equals(tmp.getId()))) {
+
+                        chatMessages.add(tmp);
+                        wasNewMessage = true;
+                    }
                 }
 
-                chatMessages.sort(new ChatDateComparator());
+                if(wasNewMessage) {
+                    //chatMessages.sort(new ChatDateComparator());
+                    //chatMessages.sort((m1, m2) -> m1.getMoment().compareTo(m2.getMoment()));
+                    chatMessages.sort(Comparator.comparing(ChatMessage::getMoment));
+                    runOnUiThread(this::showChatMessages);
+                }
             }
             else {
                 Log.d("parseChatMessages", "Content has no 'data' " + loadedContent);
@@ -104,28 +123,99 @@ public class ChatActivity extends AppCompatActivity {
         catch (JSONException ex) {
             Log.d("parseChatMessages", ex.getMessage());
         }
-
-        runOnUiThread(this::showChatMessages);
     }
 
     private void showChatMessages() {
 
-        StringBuilder sb = new StringBuilder();
+        Drawable otherBg = AppCompatResources.getDrawable(getApplicationContext(), R.drawable.chat_msg_bg_other);
+
+        LinearLayout.LayoutParams marginOther = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        marginOther.setMargins(10, 10, 10, 10);
+
+        boolean wasNewMessage = false;
 
         for(ChatMessage message : chatMessages) {
-            sb.append(message.getAuthor())
-                    .append(" : ")
-                    .append(message.getTxt())
-                    .append(" : ")
-                    .append(message.getMoment())
-                    .append('\n');
+
+            if(message.getView() != null) {
+                continue;
+            }
+
+            TextView tvMessage = new TextView(ChatActivity.this);
+            tvMessage.setText(message.toViewString());
+            tvMessage.setTag(message);      // связываем View с ChatMessage
+            message.setView(tvMessage);
+
+            tvMessage.setBackground(otherBg);
+            tvMessage.setLayoutParams(marginOther);
+            tvMessage.setTextSize(18);
+            tvMessage.setPadding(10, 7, 10, 7);
+
+            chatContainer.addView(tvMessage);
+
+            wasNewMessage = true;
         }
 
-        tvChat.setText(sb.toString());
+        if(wasNewMessage) {
+            svContainer.post(() -> svContainer.fullScroll(View.FOCUS_DOWN));
+        }
     }
 
     private void sendMessageClick(View view) {
 
+        new Thread(this::postChatMessage).start();
+    }
+
+    private void postChatMessage() {
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setTxt(etMessage.getText().toString());
+        chatMessage.setAuthor(etAuthor.getText().toString());
+
+        try {
+
+            URL chatUrl = new URL(CHAT_URL);
+
+            HttpURLConnection connection = (HttpURLConnection)chatUrl.openConnection();
+            connection.setDoOutput(true);   // будет иметь тело (output)
+            connection.setDoInput(true);    // будет иметь возврат (response)
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "*/*");
+            connection.setChunkedStreamingMode(0); // не разделять на чанки (фрагменты)
+
+            OutputStream body = connection.getOutputStream();
+            body.write(chatMessage.toJsonString().getBytes());
+            body.close();
+
+            int responseCode = connection.getResponseCode();
+
+            if(responseCode >= 400) {
+                Log.e("postChatMessage", "responseCode = " + responseCode);
+                return;
+            }
+
+            InputStream response = connection.getInputStream();
+
+            ByteArrayOutputStream byteBuilder = new ByteArrayOutputStream();
+            byte[] chunk = new byte[4096];
+            int len;
+
+            while((len = response.read(chunk)) != -1) {
+                byteBuilder.write(chunk, 0, len);
+            }
+
+            String responseText = byteBuilder.toString();
+            Log.d("postChatMessage", responseText);
+
+            byteBuilder.close();
+            response.close();
+            connection.disconnect();
+
+            new Thread(this::getChatMessages).start();
+        }
+        catch (Exception ex) {
+            Log.e("postChatMessage", ex.getMessage());
+        }
     }
 
     private static class ChatMessage {
@@ -136,6 +226,8 @@ public class ChatActivity extends AppCompatActivity {
         private Date moment;
         private UUID idReply;
         private String replyPreview;
+
+        private View view;
 
         private static final SimpleDateFormat chatMomentFormat = new SimpleDateFormat("MMM dd, yyyy KK:mm:ss a", Locale.US);
         public ChatMessage() {
@@ -161,6 +253,25 @@ public class ChatActivity extends AppCompatActivity {
             if(jsonObject.has("replyPreview")) {
                 this.setReplyPreview(jsonObject.getString("replyPreview"));
             }
+        }
+
+        public String toJsonString() {
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("{\"author\": \"%s\", \"txt\": \"%s\"", this.getAuthor(), this.getTxt()));
+
+            if(idReply != null) {
+                sb.append(String.format(", \"idReply\": \"%s\"", this.getIdReply()));
+            }
+
+            sb.append("}");
+
+            return sb.toString();
+        }
+
+        public String toViewString() {
+
+            return String.format("%s: %s", this.getAuthor(), this.getTxt());
         }
 
         // region Accessors
@@ -211,6 +322,14 @@ public class ChatActivity extends AppCompatActivity {
 
         public void setReplyPreview(String replyPreview) {
             this.replyPreview = replyPreview;
+        }
+
+        public View getView() {
+            return view;
+        }
+
+        public void setView(View view) {
+            this.view = view;
         }
 
         // endregion
